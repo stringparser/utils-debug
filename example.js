91b60c9a1ff2505aa5589cf8e8c63005ec9749b5
Javier Carrillo <stringparser@gmail.com>
2014-10-06 18:13:26 +0200

    initial commit

---
+++ example.js
@@ -0,0 +1,9 @@
+var debug = require('./.').enable();
+
+console.log('hello!');
+
+origin();
+
+function origin(){
+  console.log('what up!');
+}
--- index.js
+++ index.js
@@ -1,21 +1,68 @@
+'use strict';
 
 var path = require('path');
 var callers = require('v8-callsites');
 var monkey = require('stdout-monkey')();
+var ws = require('fs').createWriteStream('log');
 
 var wd = process.cwd();
 var registry = { };
 
-function debug(line){
-  var frame = callers(console.log);
-  var file = path.relative(wd, frame.getFileName());
+var batch = { };
+function spy(line){
+  var frame = callers(console.log)[0];
 
+  var file = path.relative(wd, frame.getFileName()) || './.';
   if( registry[file] === void 0 ){
     monkey.write(line);
     return ;
   }
 
-  var badge = ' '+caller.location+' ';
+  frame = (' at '+frame+'').replace(wd, '.')+'\n';
 
+  batch.file = batch.file || file;
+  batch.frame = batch.frame || frame;
+
+  if( batch.timer ){
+    clearTimeout(batch.timer);
+    delete batch.timer;
+  }
+
+  if( batch.frame !== frame ){
+    monkey.write(batch.frame);
+    monkey.write(batch.data.join(''));
+    batch = {
+       file : file,
+      frame : frame
+    };
+  }
+
+  batch.data = batch.data || [];
+  if( batch.data.indexOf(frame) < -1 ){
+    batch.data.push(frame);
+  }
+  batch.data.push('  '+line);
+
+  ws.write('waiting...\n'+ JSON.stringify(batch, null, ' ')+'\n');
+
+  batch.timer = setTimeout(function(){
+    if(batch.data){
+      monkey.write(batch.frame);
+      monkey.write(batch.data.join(''));
+      delete batch.timer;
+      ws.write('timer kicks\n'+ JSON.stringify(batch, null, ' ')+'\n');
+      batch = { };
+    }
+  });
 }
-monkey.patch(debug);
+monkey.patch(spy);
+
+function enable(){
+  var file = callers(enable)[0].getFileName();
+  registry[path.relative(wd, file) || './.'] = true;
+  return exports;
+}
+
+exports = module.exports = {
+  enable : enable
+};

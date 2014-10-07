a9cc474140af7effbcea17beec71bee844997d23
Javier Carrillo <stringparser@gmail.com>
2014-10-07 14:19:11 +0200

    changes

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 'use strict';
 
-var merge = require('lodash.merge');
 var site = require('v8-callsites');
 var monkey = require('stdout-monkey')();
+var prettyTime = require('pretty-hrtime');
 
 var batch = { };
 var util = require('./lib/utils');
 
-function stdout(line){
+function debug(line){
 
   var frame = site(console.log)[0];
   var file = util.relative(frame.getFileName());
 
-  if( util.skip(file, monkey, line) ){
+  if( !util.registry(file) ){
+    return monkey.write(line);
+  }
+
+  if( util.skip(file) ){
     return ;
   }
 
-  var shot = util.relative('at '+frame) + '\n';
-  batch.data = batch.data || [shot];
-  batch.file = batch.file || file;
-  batch.shot = batch.shot || shot;
+  batch.time = batch.time || process.hrtime();
+  var shot = util.relative('at '+frame + '\n');
 
-  if( util.regexpOf(batch.data, shot, monkey) < 0 ){
-      batch.data.push(shot);
+  batch.file = batch.file || file;
+  batch.data = batch.data || [];
+
+  if( util.regexpOf(batch.data, shot) < 0 ){
+    batch.data.push(shot);
   }
 
   if( batch.file !== file ){
     monkey.write(batch.data.join(''));
-    batch = { file : file, shot : shot };
+    batch = {
+      file : file,
+      time : process.hrtime()
+    };
     return ;
   }
 
-  batch.data.push(line);
+  batch.data.push(' '+line);
 
   clearTimeout(batch.timer);
   delete batch.timer;
@@ -43,13 +51,21 @@ function stdout(line){
   });
 }
 
-monkey.patch(stdout);
+monkey.patch(debug);
 
-process.on('exit', function(){
+process.once('exit', function(){
   if(batch.data){
     monkey.write(batch.data.join(''));
   }
 });
 
-exports = module.exports = merge({ }, monkey);
-exports.enable = util.enable;
+function enable(){
+  util.register(enable);
+  return module.exports;
+}
+
+module.exports =  {
+   enable : enable,
+   monkey : monkey,
+    write : debug
+};
